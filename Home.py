c94ad341ed30c8fe47d046d499da2a04cdf381c0
Anupa-123 <anupadevda3@gmail.com>
2024-06-06 17:04:56 +0530

    changes in home

--- Home.py
+++ Home.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title='Attendance System',layout='wide')
 
 st.header('Attendance System using Face Recognition')
 
+
 with st.spinner("Loading Models and Conneting to Redis db ..."):
     import face_rec
     
